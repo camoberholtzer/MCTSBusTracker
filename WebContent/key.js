a9f3aef9d0f36f129ede0a4808adba86093a6b1a
Kyra Oberholtzer <kyra_oberholtzer@yahoo.com>
2017-03-09 15:50:28 -0600

    Update key.js

--- WebContent/key.js
+++ WebContent/key.js
@@ -1,6 +1,5 @@
 /**
  * Kyra Oberholtzer, Lab 5
- * Created by hornick on 4/12/2016.
  * This file contains your http://realtime.ridemcts.com developer's key
  */
-var key = "u4J4yTgsRbAeA6RFW8YpfUZNR";
+var key = "ABCDEFGHIJK"; //This is a sample key and real MCTS key will need to be provided for this project to function correctly
